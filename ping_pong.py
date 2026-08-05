b9da71c8693d1770859b3a4bb162e50b0e34b7ec
SofiayOvchinnikova <so4915459@gmail.com>
2026-08-05 20:48:39 +0400

    Update ping_pong.py

--- ping_pong.py
+++ ping_pong.py
@@ -1,5 +1,6 @@
 from pygame import *
 mixer.init()
+font.init()
 import random
 import os
 import sys
@@ -21,7 +22,7 @@ class Igroc(sprite.Sprite):
         self.rect.y = y
     def reset(self, window):#отрисовывает игроя в точке
         window.blit(self.image, (self.rect.x, self.rect.y))
-        #draw.rect(window, (0,0,0), self.rect, 3)
+        #draw.rect(window, (255,255,255), self.rect, 3)
 
 class Raketa(Igroc):
     def update_r(self): # проверяет куда идти
@@ -36,15 +37,26 @@ class Raketa(Igroc):
             self.rect.y -= self.skorost
         elif keys_pressed[K_s] and self.rect.bottom < 800:
             self.rect.y += self.skorost
-
+class Mach(Igroc):
+    def __init__(self, x, y, width, height, skorost, filename):
+        super().__init__(x, y, width, height, skorost, filename)
+        self.skorost_x = self.skorost
+        self.skorost_y = self.skorost
+    def update(self):
+        self.rect.y += self.skorost_y
+        self.rect.x += self.skorost_x
+        if self.rect.bottom >= win_h:
+            self.skorost_y = (-1) * abs(self.skorost_y)
+        elif self.rect.y <= 0:
+            self.skorost_y = abs(self.skorost_y)
 win_w = 1200
 win_h = 800
 window = display.set_mode((win_w, win_h))#создание окна игры
 background = transform.scale(image.load(poisk_kart("galaxy.jpg")), (win_w, win_h))
 
-raketka = Raketa(100, 310, 150, 280, 4, "racketka.png")
-raketka2 = Raketa(900, 310, 150, 280, 4, "racketka.png")
-mach  = Raketa(510, 370, 120, 80, 4, "ufo.png")
+raketka = Raketa(100, 310, 104, 154, 4, "image.png")
+raketka2 = Raketa(900, 310, 104, 154, 4, "image.png")
+mach  = Mach(510, 370, 120, 80, 3, "ufo.png")
 
 vragi = sprite.Group()
 
@@ -68,14 +80,25 @@ while game:#игра идет
     if not stop:
         raketka.update_l()
         raketka2.update_r()
-        if sprite.collide_rect(mach, raketka) or sprite.collide_rect(mach, raketka2):
-            mach.skorost_x *= -1
+        mach.update()
+        if sprite.collide_rect(mach, raketka):
+            mach.skorost_x = +1 * abs(mach.skorost_x)
+        if sprite.collide_rect(mach, raketka2):
+            mach.skorost_x = -1 * abs(mach.skorost_x)
         window.blit(background, (0, 0))
         raketka.reset(window)
         raketka2.reset(window)
         mach.reset(window)
-            #------------
-
+        
+        if mach.rect.left < 0:
+            stop = True
+            text_lose = font.SysFont('verdana', 70).render('выигрыш правого', True, (200, 50, 50))
+            window.blit(text_lose, (140, 150))
+        elif mach.rect.right > win_w:
+            stop = True
+            text_lose = font.SysFont('verdana', 70).render('выигрыш левого', True, (50, 200, 50))
+            window.blit(text_lose, (140, 150))
+#------------
     display.update()
     clock.tick(FPS)
 quit()
